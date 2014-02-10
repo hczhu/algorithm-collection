628451e60d87cbfb5bdadfa1a6da126a950660e3
hcz <hcz@fb.com>
2014-02-10 13:55:27 +0000

    Add two more c++11 feature related files.

---
+++ C++11/unique-ptr.cc
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <map>
+#include <cstring>
+#include <complex>
+#include <numeric>
+#include <functional>
+#include <cassert>
+#include <limits>
+#include <limits.h>
+#include <cstdarg>
+#include <iterator>
+#include <valarray>
+#include <thread>
+#include <memory>
+using namespace std;
+#define debug(x) cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<<endl;
+struct Int {
+  Int(int v) : v_(v) {}
+  int operator()()const {
+    return v_;
+  }
+  ~Int() {
+    cout << "Dtor is called for v_ = "  << v_ << endl;
+  }
+  int v_;
+};
+
+int main() {
+  map<int, unique_ptr<Int>> mem;
+  mem.insert(make_pair(1, unique_ptr<Int>(new Int(1))));
+  mem[0] = unique_ptr<Int>(new Int(0));
+  cout << mem[0]->v_ << endl;
+  auto itr = mem.find(1);
+  cout << itr->second->v_ << endl;
+  mem.erase(itr);
+
+  vector<unique_ptr<Int>> vec;
+  vec.emplace_back(new Int(2));
+  vec.emplace_back(new Int(3));
+  vec.push_back(std::move(vec[0]));
+  cout << vec[0].get() << endl;
+  return 0;
+}

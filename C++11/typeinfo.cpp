528d4744b1ef06516e3676ea223ed2fa597b377b
hcz <hcz@fb.com>
2014-05-16 15:13:31 +0100

    Minor changes.

--- C++11/typeinfo.cpp
+++ C++11/typeinfo.cpp
@@ -25,18 +25,36 @@
 using namespace std;
 #define debug(x) cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<<endl;
 class Example {
+ public:
+  void Foo() {}
+  void ConstFoo() const {}
 };
 
 int main() {
+  const unique_ptr<Example> ptr(new Example);
+  ptr->Foo();
+  using Map = map<Example, Example>;
   using ConstMap = const map<Example, Example>;
   using MapConst = map<Example, const Example>;
   ConstMap constMap;
   MapConst mapConst;
-  cout << typeid(mapConst.begin()->first).name() << std::endl;
-  cout << typeid(constMap.begin()->second).name() << std::endl;
+  Map myMap;
+  const auto& constRef = myMap;
+  cout << is_const<decltype(constRef)>::value << endl;
+  cout << "--------------" << endl;
+  cout << typeid(myMap.begin()->first).hash_code() << std::endl;
+  cout << typeid(myMap.begin()->second).hash_code() << std::endl;
+  cout << is_const<decltype(myMap.begin()->second)>::value << std::endl;
+  cout << is_reference<decltype(myMap.begin()->second)>::value << std::endl;
+  cout << "--------------" << endl;
+  cout << typeid(mapConst.begin()->first).hash_code() << std::endl;
+  cout << typeid(constMap.begin()->second).hash_code() << std::endl;
   cout << is_const<decltype(constMap.begin()->second)>::value << std::endl;
-  cout << typeid(mapConst.begin()->first).name() << std::endl;
-  cout << typeid(mapConst.begin()->second).name() << std::endl;
+  cout << is_reference<decltype(myMap.begin()->second)>::value << std::endl;
+  cout << "--------------" << endl;
+  cout << typeid(mapConst.begin()->first).hash_code() << std::endl;
+  cout << typeid(mapConst.begin()->second).hash_code() << std::endl;
   cout << is_const<decltype(mapConst.begin()->second)>::value << std::endl;
+  cout << is_const<const Example&>::value << endl;
   return 0;
 }

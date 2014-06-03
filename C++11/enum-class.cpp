e5e5ef114a8c6c5578651183ac8c89f11efeb72f
hcz <hcz@fb.com>
2014-06-03 10:43:20 +0100

    Add new files.

---
+++ C++11/enum-class.cpp
@@ -0,0 +1,38 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <map>
+#include <cstring>
+#include <complex>
+#include <numeric>
+#include <functional>
+#include <cassert>
+#include <limits>
+#include <limits.h>
+#include <cstdarg>
+#include <iterator>
+#include <valarray>
+#include <thread>
+#include <memory>
+using namespace std;
+#define debug(x) cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<<endl;
+
+enum class Animal {
+  CAT,
+  DOG, 
+};
+
+int main() {
+  int a[100];
+  a[Animal::CAT] = 1;
+  a[Animal::DOG] = 1;
+  return 0;
+}
